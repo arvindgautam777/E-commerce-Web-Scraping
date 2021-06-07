6b5bbdd4b5e0d2b81143d9f8062beab97432270e
ARVIND R GAUTAM <67141403+arvindgautam777@users.noreply.github.com>
2021-06-07 19:49:53 +0530

    Update all_retailer.py

--- scripts/python/all_retailer.py
+++ scripts/python/all_retailer.py
@@ -2,7 +2,7 @@
 ##########################################################################################################################
 #  Created Date: 05/06/2020
 #  Author : Arvind Gautam
-#  Purpose of this Script: The main objective of this POC is to extract the product related information for JNJ products on
+#  Purpose of this Script: The main objective of this POC is to extract the product related information for products on
 #                              various e-commerce websites like Amazon, Walmart  using Python web scraping.
 #                          Inputs in the form of URL will be used in the POC and CSV file with the listed details
 #                              should be generated. POC should be able to handle large number of URLs.
